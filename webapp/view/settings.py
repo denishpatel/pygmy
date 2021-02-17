ad4918d958e229770f05d212fd957b10893d6f7e
Aditya Mehta <aadityamehta15@gmail.com>
2021-02-17 21:30:54 +0530

    Changes: 1. Fixed a scenario where multiple settings of same name (rds, ec2, etc) were getting created

--- webapp/view/settings.py
+++ webapp/view/settings.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
-
 from engine.aws_wrapper import AWSData
 from webapp.models import Settings
 
