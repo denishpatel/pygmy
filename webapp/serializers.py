445e28bf81b7fe28933c97eab0fe292134b01123
Aditya Mehta <aadityamehta15@gmail.com>
2021-04-03 19:43:23 +0530

    Changes: 1. Added redoc swagger documentation generation of APIs

--- webapp/serializers.py
+++ webapp/serializers.py
@@ -1,2 +1,32 @@
-class CombineView(serializer.Serializer):
-    pass
+from rest_framework import serializers
+from engine.models import Rules, ClusterInfo, ExceptionData
+
+
+class RuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rules
+        fields = ['id', 'name', 'cluster', 'action', 'rule', 'run_at']
+
+
+class RuleCreateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=32)
+    cluster_id = serializers.CharField(max_length=64)
+    action = serializers.CharField(max_length=64)
+    typeTime = serializers.ChoiceField(choices=["daily", "cron"])
+
+
+class ClusterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClusterInfo
+        fields = ['id', 'name', 'primaryNodeIp', 'type']
+
+
+class ExceptionDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExceptionData
+        fields = ['id', 'exception_date', 'clusters', 'added_on', 'updated_on']
+
+
+class ExceptionCreateSerializer(serializers.Serializer):
+    dates = serializers.JSONField(help_text="list of valid dates of format YYYY-MM-DD", default=list)
+    clusterIds = serializers.JSONField(help_text="list of valid cluster ids", default=list)
